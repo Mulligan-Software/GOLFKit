32c59485173f72d1030f7e52ca8d796eb6bdf3c1
MulliganGolf <jb@mulligansoftware.com>
2016-11-05 13:24:46 -0500

    Initial Commit

---
+++ GOLFKit/GOLFKit/GOLFKit.h
@@ -0,0 +1,19 @@
+//
+//  GOLFKit.h
+//  GOLFKit
+//
+//  Created by John Bishop on 11/5/16.
+//  Copyright © 2016 Mulligan Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GOLFKit.
+FOUNDATION_EXPORT double GOLFKitVersionNumber;
+
+//! Project version string for GOLFKit.
+FOUNDATION_EXPORT const unsigned char GOLFKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GOLFKit/PublicHeader.h>
+
+
---
+++ GOLFKit/GOLFKitTests/GOLFKitTests.m
@@ -0,0 +1,39 @@
+//
+//  GOLFKitTests.m
+//  GOLFKitTests
+//
+//  Created by John Bishop on 11/5/16.
+//  Copyright © 2016 Mulligan Software. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GOLFKitTests : XCTestCase
+
+@end
+
+@implementation GOLFKitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
